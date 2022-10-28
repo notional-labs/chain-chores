38c8990750733b5bd8eebcb131dc4386e3287991
Reece Williams <reecepbcups@gmail.com>
2022-10-28 14:18:55 -0500

    update chains we validate

--- main.py
+++ main.py
@@ -22,78 +22,162 @@ VERSION BUMPING:
 '''
 
 # chains real repos (put notional repos here in the future so we have write access)
-'''
-{
-    "Osmosis": "git@github.com:osmosis-labs/osmosis.git",
-    "Cosmoshub": "git@github.com:cosmos/gaia.git",
-    "Juno": "git@github.com:CosmosContracts/juno.git",
-    "Sifchain": "git@github.com:Sifchain/sifnode.git",
-    "Omniflix": "git@github.com:OmniFlix/omniflixhub.git",
-    "Secret-Network": "git@github.com:scrtlabs/SecretNetwork.git",
-    "Starname": "",
-    "Regen": "git@github.com:regen-network/regen-ledger.git",
-    "Akash": "git@github.com:ovrclk/akash.git",
-    "Sentinel": "git@github.com:sentinel-official/sentinel.git",
-    "E-Money": "git@github.com:e-money/em-ledger.git",
-    "Ixo": "git@github.com:ixofoundation/ixo-blockchain.git",
-    # "Likecoin": "",
-    # "Ki": "",
-    "Cyber": "git@github.com:cybercongress/go-cyber.git",
-    "Cheqd": "git@github.com:cheqd/cheqd-node.git",
-    "Stargaze": "",
-    "Band": "git@github.com:bandprotocol/chain.git",
-    # "Chihuahua": "",
-    # "Kava": "",
-    # "BitCanna": "",
-    # "Konstellation": "",
-    # "Evmos": "",
-    # "Provenance": "",
-    # "Vidulum": "",
-    # "Bitsong": "",
-    # "Fetch.AI": "",
-    # "Umee": "",
-    # "Injective": "",
-    # "Passage3D": "",
-    "Stride": "git@github.com:Stride-Labs/stride.git",
-    # "QuickSilver": "",
-    # "Jackal": "",
-}
-'''
 
+# ToDO: if we do not have a repo, we should auto fork it for notional?
 VALIDATING_CHAINS = {
-    # "Osmosis": {
+    # "Osmosis": { # currently protected main
+    #     "branch": "main",
     #     "root": "git@github.com:osmosis-labs/osmosis.git",
     #     "notional": "git@github.com:notional-labs/osmosis.git"
     # },
-    # "Cosmoshub": {
+    # "Gaia": {
+    #     "branch": "main",
     #     "root": "git@github.com:cosmos/gaia.git",
     #     "notional": "git@github.com:notional-labs/gaia.git"
+    # },    
+    # "Juno": { 
+    #     "branch": "main",
+    #     "root": "git@github.com:CosmosContracts/juno.git",
+    #     "notional": "git@github.com:notional-labs/juno.git"
     # },
-    # "Juno": "git@github.com:CosmosContracts/juno.git",
-    # "Sifchain": "git@github.com:Sifchain/sifnode.git",
+    # "Sifchain": { 
+    #     "branch": "master",
+    #     "root": "git@github.com:Sifchain/sifnode.git",
+    #     "notional": "git@github.com:notional-labs/sifnode.git"
+    # },    
     # "Omniflix": {
+    #     "branch": "main",
     #     "root": "git@github.com:OmniFlix/omniflixhub.git",
-    #     "notional": "git@github.com:notional-labs/mainnet-1.git"
+    #     "notional": "git@github.com:notional-labs/omniflixhub.git"
     # },
+    # "Secret": {
+    #     "branch": "master",
+    #     "root": "git@github.com:scrtlabs/SecretNetwork.git",
+    #     "notional": "git@github.com:notional-labs/SecretNetwork.git"
+    # },
+    "Starname-IOV": {
+        "branch": "master",
+        "root": "git@github.com:iov-one/starnamed.git",
+        "notional": "git@github.com:notional-labs/starnamed.git"
+    },
+    "Regen": {
+        "branch": "master",
+        "root": "git@github.com:regen-network/regen-ledger.git",
+        "notional": "git@github.com:notional-labs/regen-ledger.git"
+    },
+    "Akash": {
+        "branch": "master",
+        "root": "git@github.com:ovrclk/akash.git",
+        "notional": "git@github.com:notional-labs/akash.git"
+    },
+    "Sentinel": {
+        "branch": "master",
+        "root": "git@github.com:sentinel-official/sentinel.git",
+        "notional": "git@github.com:notional-labs/sentinel.git"
+    },
+    "Emoney": {
+        "branch": "develop",
+        "root": "git@github.com:e-money/em-ledger.git",
+        "notional": "git@github.com:notional-labs/em-ledger.git"
+    },
+    "Emoney": {
+        "branch": "main",
+        "root": "git@github.com:ixofoundation/ixo-blockchain.git",
+        "notional": "git@github.com:notional-labs/ixo-blockchain.git"
+    },
+    "LikeCoin": {
+        "branch": "master",
+        "root": "git@github.com:likecoin/likecoin-chain.git",
+        "notional": "git@github.com:notional-labs/likecoin-chain.git"
+    },
     # "Cyber": {
     #     "branch": "main",
     #     "root": "git@github.com:cybercongress/go-cyber.git",
     #     "notional": "git@github.com:notional-labs/go-cyber.git"
     # },
+    "Cheqd": {
+        "branch": "main",
+        "root": "git@github.com:cheqd/cheqd-node.git",
+        "notional": "git@github.com:notional-labs/cheqd-node.git"
+    },
+    "Stargaze": {
+        "branch": "main",
+        "root": "git@github.com:public-awesome/stargaze.git",
+        "notional": "git@github.com:notional-labs/stargaze.git"
+    },
+    "BandChain": {
+        "branch": "master",
+        "root": "git@github.com:bandprotocol/chain.git",
+        "notional": "git@github.com:notional-labs/chain.git"
+    },
+    "Chihuahua": {
+        "branch": "main",
+        "root": "git@github.com:ChihuahuaChain/chihuahua.git",
+        "notional": "git@github.com:notional-labs/chihuahua.git"
+    },
+    "Kava": {
+        "branch": "master",
+        "root": "git@github.com:Kava-Labs/kava.git",
+        "notional": "git@github.com:notional-labs/kava.git"
+    },
+    "BitCanna": {
+        "branch": "main",
+        "root": "git@github.com:BitCannaGlobal/bcna.git",
+        "notional": "git@github.com:notional-labs/bcna.git"
+    },
+    "Evmos": {
+        "branch": "main",
+        "root": "git@github.com:evmos/evmos.git",
+        "notional": "git@github.com:notional-labs/evmos.git"
+    },
+    "Provenance": {
+        "branch": "main",
+        "root": "git@github.com:provenance-io/provenance.git",
+        "notional": "git@github.com:notional-labs/provenance.git"
+    },
+    "Vidulum": {
+        "branch": "main",
+        "root": "git@github.com:vidulum/mainnet.git",
+        "notional": "git@github.com:notional-labs/vidulum.git"
+    },
+    "Bitsong": {
+        "branch": "main",
+        "root": "git@github.com:bitsongofficial/go-bitsong.git",
+        "notional": "git@github.com:notional-labs/go-bitsong.git"
+    },
+    "FetchAI": {
+        "branch": "master",
+        "root": "git@github.com:fetchai/fetchd.git",
+        "notional": "git@github.com:notional-labs/fetchd.git"
+    },
+    "Uume": {
+        "branch": "main",
+        "root": "git@github.com:umee-network/umee.git",
+        "notional": "git@github.com:notional-labs/umee.git"
+    },
+    "Passage3D": {
+        "branch": "main",
+        "root": "git@github.com:envadiv/Passage3D.git",
+        "notional": "git@github.com:notional-labs/Passage3D.git"
+    },
+    "Stride": {
+        "branch": "main",
+        "root": "git@github.com:Stride-Labs/stride.git",
+        "notional": "git@github.com:notional-labs/stride.git"
+    },
+    "QuickSilver": {
+        "branch": "main",
+        "root": "git@github.com:ingenuity-build/quicksilver.git",
+        "notional": "git@github.com:notional-labs/quicksilver.git"
+    },
     "Jackal": {
         "branch": "master",
         "root": "git@github.com:JackalLabs/canine-chain.git",
         "notional": "git@github.com:notional-labs/canine-chain.git"
     },
-    # "Secret-Network": "git@github.com:scrtlabs/SecretNetwork.git",
-    # "Regen": "git@github.com:regen-network/regen-ledger.git",
-    # "Akash": "git@github.com:ovrclk/akash.git",
-    # "Sentinel": "git@github.com:sentinel-official/sentinel.git",
-    # "E-Money": "git@github.com:e-money/em-ledger.git",
-    # "Ixo": "git@github.com:ixofoundation/ixo-blockchain.git",    
-    # "Cheqd": "git@github.com:cheqd/cheqd-node.git",
-    # "Band": "git@github.com:bandprotocol/chain.git",
-
+    # # "Injective": "",    
+    # # "Ki": "", # ??  
+    # # "Konstellation": "",
 }
 
 import os
@@ -112,25 +196,26 @@ def pull_latest(folder_name):
     # git clone if it is not there already, if it is, cd into dir and git pull    
     os.chdir(current_dir)
 
+    parent_link = VALIDATING_CHAINS[folder_name]["root"]    
     repo_link = VALIDATING_CHAINS[folder_name]["notional"]    
 
     # check if folder_name exists
     if os.path.isdir(os.path.join(current_dir, folder_name)):        
         print(f"pulling latest {folder_name}")
         os.chdir(folder_name)
-        os.system("git pull")
+        os.system("git pull origin")
         # os.chdir("..")
     else:
         # run a system command to get git clone
         print(f"Cloning {folder_name} from {repo_link}")
-        os.system(f"git clone {repo_link} {folder_name}")
+        os.system(f"git clone {repo_link} {folder_name}")    
 
-
-    # Ex: gh repo sync notional-labs/go-cyber (gets latest synced branchs up to date)
-    # force pulls an overwrite on main/master branch
-    print(f"Syncing fork to latest of parent main/master branch (overwriting any main/master changes)")
-    # ✓ Synced the "notional-labs:main" branch from "osmosis-labs:main"
+    # print(f"Syncing fork to latest of parent main/master branch (overwriting any main/master changes)")    
+    os.system(f"git remote add upstream {repo_link}") # if not already
     os.system(f"gh repo sync {repo_link.replace('git@github.com:', '').replace('.git', '')} --force")
+
+    # pull after we sync to the new files in the notional repo which we will build off of
+    os.system("git pull")
 
     os.chdir(current_dir)
 
@@ -198,51 +283,43 @@ def workflows(folder_name):
 
 
 
-def go_mod_update(folder_name):
-    OLD = "github.com/cosmos/ibc-go/v3 v3.0.0"
-    NEW = "github.com/cosmos/ibc-go/v3 v3.3.1"
+def go_mod_update(folder_name, values: list[str] = []):
     os.chdir(os.path.join(current_dir, folder_name))
     
     # read go.mod data
     with open("go.mod", "r") as f:
         data = f.read()
+        
+    for replacements in values:
+        # print(f"Replacing {replacements[0]} with {replacements[1]}")
+        data = data.replace(replacements[0], replacements[1])
+    # print(data)
 
-    # replace OLD with NEW
-    data = data.replace(OLD, NEW)
-
-    # write data to go.mod
     with open("go.mod", "w") as f:
+        print(f"Writing go.mod for {folder_name}")
         f.write(data)
-
-    # go vet ./...
 
     os.system("go mod tidy")
     os.chdir(current_dir)
 
-    
-class Panel:
-    def __init__():
-        pass
-    # like the MC panel, each section allow for modifying all chains
 
 
-
-
+PATCH_BRANCH_NAME = "reece-ibc331" # this is made on notionals fork
 def oct_28_2022_patches():
-    '''
-    Loop through all chains we validate,
-    '''
     for chain, loc in get_chains():
-        print(f"Chain: {chain}. Location: {loc}")
-        # pull_latest(chain)
+        print(f"Chain: {chain}")
+        pull_latest(chain)
         # dependabot(chain)
         # workflows(chain)
-        # go_mod_update(chain)
-        lint(chain)
+
+        # go_mod_update(chain, [
+        #     # allow regex matching here? so we could do v3.*.* -> v3.3.1
+        #     ["github.com/cosmos/ibc-go/v3 v3.3.0", "github.com/cosmos/ibc-go/v3 v3.3.1"]
+        # ])
+
+        # lint(chain)
 
 
 
 
 oct_28_2022_patches()
-
-# pull_latest("Cyber")
